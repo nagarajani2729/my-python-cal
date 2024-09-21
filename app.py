7e31d5494680c1ac7697ff911ecf38747b145556
rajanisai2729 <rajanigowthu2903@gmail.com>
2024-09-21 14:30:56 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by Zero."
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operation = request.form['operation']
+
+        if operation == 'add':
+            result = add(num1, num2)
+        elif operation == 'subtract':
+            result = subtract(num1, num2)
+        elif operation == 'multiply':
+            result = multiply(num1, num2)
+        elif operation == 'divide':
+            result = divide(num1, num2)
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
